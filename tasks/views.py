c466fd01ec35c0c1707510f88b6e8622780fcf9d
Your Name <you@example.com>
2025-01-23 00:23:17 +0300

    latest changes

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseForbidden
 #from django.template import loader
 from .models import Task
 from .forms import TaskForm
@@ -14,9 +15,26 @@ def index(request):
 
 def tasks(request):
     context = {}
-    form = TaskForm
+
     tasks = Task.objects.all().values
     context['tasks'] = tasks
     context['title'] = 'tasks'
-    context['form']  =  form
     return render(request, 'tasks.html',context)
+
+
+def save(request):
+    # Ensure access is via POST only
+    # if request.method != "POST":
+    #      return HttpResponseForbidden("Access denied.")
+    
+    context = {}
+    form = TaskForm()
+    context['title'] = 'save'
+    if request.method == "POST":
+        if 'save' in request.POST:
+            form = TaskForm(request.POST)
+            form.save()
+             
+    context['form']  =  form
+
+    return render(request, 'save.html',context)
