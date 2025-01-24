41d9ac4be42f1286ed57e1449f65b2f861784b27
Maritimsephania <kipkoechsephania.gmail.com>
2025-01-24 22:02:51 +0300

    2401 changes

--- tasks/forms.py
+++ tasks/forms.py
@@ -4,4 +4,8 @@ from tasks.models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title','description', 'due_date']
+
+        fields = ['title','description','due_date']
+        due_date = forms.DateField(
+            widget=forms.DateInput(attrs={'type':'date'}),
+        )
--- tasks/urls.py
+++ tasks/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('tasks/', views.tasks, name='tasks'),
     path('save/', views.save, name='save'),
+    #path('save/<int:task_id>',views.save,name='save_with_id'),
 
     #path('index/update', views.tasks, name='update_task'),
     
